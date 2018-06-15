25277a40f7e0eb6b3b3d8efcf2e7459648727591
Ignacio <shoik.xtazy@gmail.com>
2018-06-15 18:20:12 -0400

    Verificar si cedula chilena está Vigente

---
+++ anotter.js
@@ -0,0 +1,38 @@
+var casper = require('casper').create({
+	clientScripts: ['jquery.min.js']
+});
+//utils para setear argumentos
+var utils = require("utils");
+var url = "https://portal.sidiv.registrocivil.cl/usuarios-portal/pages/DocumentRequestStatus.xhtml";
+
+casper.start(url,function(){
+    this.echo(this.getTitle());
+});
+
+casper.waitForResource(url,function() {
+    this.fillSelectors('#form', {
+        //'input[name="form:run"]' : '17899729-7',
+        'input[name="form:run"]' : casper.cli.args[0],
+        'select[name="form:selectDocType"]':'CEDULA',
+        //'input[name="form:docNumber"]':'514998583'
+        'input[name="form:docNumber"]':casper.cli.args[1]
+    }, true);
+});
+
+casper.then(function(){
+	this.click('a.volver');
+});
+
+casper.wait(4000,function(){
+	casper.capture("rockyoulikeanhurricane.png",{top:0,left:0,width:1200,height:720});
+	casper.echo("Lizz Taylor");
+});
+
+casper.then(function(){
+	var result = casper.evaluate(function(){
+		return $('.setWidthOfSecondColumn').text();
+	});
+	this.echo(result);
+});
+
+casper.run();
